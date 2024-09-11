2ba91de8bb7e941df8f8978d9464c00ffb376b41
Erkin <ercaprio@gmail.com>
2024-09-11 22:17:57 +0500

    added createAsyncThunk

--- src/components/heroesFilters/HeroesFilters.js
+++ src/components/heroesFilters/HeroesFilters.js
@@ -3,8 +3,7 @@ import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import classNames from 'classnames';
 
-import { fetchFilters } from '../../actions';
-import { activeFilterChanged } from './filtersSlice';
+import { activeFilterChanged, fetchFilters } from './filtersSlice';
 import Spinner from '../spinner/Spinner';
 
 const HeroesFilters = () => {
@@ -14,7 +13,7 @@ const HeroesFilters = () => {
     const {request} = useHttp();
 
     useEffect(() => {
-        dispatch(fetchFilters(request));
+        dispatch(fetchFilters());
         // eslint-disable-next-line
     }, []);
 
